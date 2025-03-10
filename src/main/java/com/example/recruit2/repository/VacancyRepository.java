10a79d78d7def4f7d52016ef3bcf5e45c17c2439
Шопоков Нурсултан <Nursultan00031@outlook.com>
2025-03-10 17:34:59 +0600

    sort

--- src/main/java/com/example/recruit2/repository/VacancyRepository.java
+++ src/main/java/com/example/recruit2/repository/VacancyRepository.java
@@ -1,16 +1,14 @@
 package com.example.recruit2.repository;
 
-import java.util.List;
-
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.recruit2.models.Vacancy;
 
 public interface VacancyRepository extends JpaRepository<Vacancy,Integer> {
         Page<Vacancy> findAll(Pageable pageable);
-        List<Vacancy> findAll(Sort sort);
+
 }
